9d33193876a57f1204896f562cb2e87e3e50f5c8
Гальцов Сергей <s.galtsov@dssl.ru>
2023-03-22 14:34:19 +0300

    add allure tittle

--- tests/cms/test_pos_report.py
+++ tests/cms/test_pos_report.py
@@ -1,7 +1,9 @@
 from prototype_auto_cloud.model.app import cms
 from prototype_auto_cloud.data.report import connect_report
+import allure
 
 
+@allure.tittle('Create report')
 def test_create_connections_report():
     create_result = 'create'
     cms.report.create(connect_report)\
